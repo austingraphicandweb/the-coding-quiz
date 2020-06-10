57e6d3c7e3713f9391f31e47ff96c3687cef100a
austinjkeener <austingraphicandweb@gmail.com>
2020-06-09 21:16:58 -0400

    javascript update

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -2,22 +2,22 @@
  * The Coding Game Javascript File
  */
 
-// Using DOM to target the quiz, start button, and score tracking section of game.
+ // Using DOM to target the quiz, start button, and score tracking section of game.
 var quiz = document.getElementById("quiz");
 var startBtn = document.getElementById("start-btn");
 var score = document.getElementById("score-tracker");
 
+// Using object method to store questions and answers
+var qna = [{}];
 
+// Function for the quiz
+function theQuiz(){}
 
+// Function to get the results of the quiz
+function theScore(){}
 
-
-
-
-
-
-
-
-
+// calling the quiz
+theQuiz();
 
 // This event listener is going to be linked to a function that is created to show the results of a user taking the quiz.
 score.addEventListener("click",);
